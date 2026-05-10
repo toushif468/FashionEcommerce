7f322b6ea82be53d5d7de1eaee3b4f880eec1edf
Toushif Muktashid Hasan <toushiforbeebro@gmail.com>
2026-05-10 12:11:34 +0600

    update: provided route for clear wishlist

--- backend/routes/wishlistRoute.js
+++ backend/routes/wishlistRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import authUser from '../middleware/auth.js'
-import { addToWishlist, clearWishlist, getUserWishlist } from '../controllers/wishlistController.js'
+import { addToWishlist, clearOne, clearWishlist, getUserWishlist } from '../controllers/wishlistController.js'
 
 const wishlistRouter = express.Router()
 
 wishlistRouter.post('/add', authUser, addToWishlist);
 wishlistRouter.post('/get', authUser, getUserWishlist);
 wishlistRouter.post('/clear-all', authUser, clearWishlist);
+wishlistRouter.post('/clear', authUser, clearOne);
 
 export default wishlistRouter
